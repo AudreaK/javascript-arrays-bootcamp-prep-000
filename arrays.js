ab3f61778aa5ad5e41c9a6987a2dd181ae6a358c
Audrea Agostini <akagostini@gmail.com>
2018-06-29 19:57:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,11 @@
-var ingrediant1 = "bread"
-var ingrediant2 = "mild cheese"
-var ingrediant3 = "sharp cheese"
-var ingrediant4 = "butter"
-var ingrediant5 = "tomato"
-var ingrediant6 = "garlic"
+var ingrediant1 = "bread";
+var ingrediant2 = "mild cheese";
+var ingrediant3 = "sharp cheese";
+var ingrediant4 = "butter";
+var ingrediant5 = "tomato";
+var ingrediant6 = "garlic";
+
+var chocolateBars = [ "snickers", "hundred grand", "kitkat", "skittles" ];
+
+function destructivelyAddElementToBeginningOfArray [ 
+  return ]
